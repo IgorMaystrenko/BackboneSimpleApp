c06811c8cc5b7565c40a703debc3461a73168535
IgorM <123@123>
2016-06-15 16:43:25 +0300

    Initialized directory.

---
+++ BackboneSimpleApp/Controllers/ContactsController.cs
@@ -0,0 +1,54 @@
+﻿using BackboneSimpleApp.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace BackboneSimpleApp.Controllers
+{
+    public class ContactsController : ApiController
+    {
+
+        private List<Contact> contacts = new List<Contact>
+        {
+            new Contact { Id = 1001, Name = "Contact #1", Phone = "+1111111111111" },
+            new Contact { Id = 1002, Name = "Contact #2", Phone = "+1111111111111" },
+            new Contact { Id = 1003, Name = "Contact #3", Phone = "+1111111111111" },
+            new Contact { Id = 1004, Name = "Contact #4", Phone = "+1111111111111" },
+            new Contact { Id = 1005, Name = "Contact #5", Phone = "+1111111111111" }
+        };
+
+        // GET api/Contacts
+        public JsonResult<List<Contact>>  GetContacts()
+        {
+            return Json(contacts);
+        }
+
+        //// GET api/Contacts/5
+        //[ResponseType(typeof(Contact))]
+        //public IHttpActionResult GetContact(int id)
+        //{
+            
+        //}
+
+        //// PUT api/Contacts/5
+        //public IHttpActionResult PutContact(int id, Contact contact)
+        //{
+        //   }
+
+        //// POST api/Contacts
+        //[ResponseType(typeof(Contact))]
+        //public IHttpActionResult PostContact(Contact contact)
+        //{
+            
+        //}
+
+        //// DELETE api/Contacts/5
+        //[ResponseType(typeof(Contact))]
+        //public IHttpActionResult DeleteContact(int id)
+        //{
+            
+        //}
+
+
+    }
+}
---
+++ BackboneSimpleApp/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace BackboneSimpleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
